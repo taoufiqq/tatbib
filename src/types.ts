dded6f88c13a1171359f329a8d46387cea889399
Taoufiq ELHANCHAOUI <t.elhanchaoui@gmail.com>
2025-04-24 20:30:31 +0100

    update file types

--- src/types.ts
+++ src/types.ts
@@ -7,41 +7,36 @@ export interface Medicine {
   login?: string; // Optional property
 }
 
-  export interface Patient {
-    firstName : string,
-    lastName : string,
-    age : number,
-    telephone : number,
-    email : string,
-    login : string,
-    password : string
-  }
+export interface Patient {
+  firstName: string;
+  lastName: string;
+  age: number;
+  telephone: number;
+  email: string;
+  login: string;
+  password: string;
+}
 
-  export interface Secretary {
-    fullName : string,
-    email : string,
-    login : string,
-    password : string,
-    status : string,
-    loginMedcine : string,
-    roleSecretary : string
-  }
+export interface Secretary {
+  fullName: string;
+  email: string;
+  login: string;
+  password: string;
+  status: string;
+  loginMedcine: string;
+  roleSecretary: string;
+}
 
-  interface AppointmentData {
-    dateTime: string;
-    medcine: string;
-    patient: string;
-    loginMedcine?: string;
-  }
-  
-  interface ApiResponse {
-    _id?: string;
-    error?: boolean;
-    message?: string;
-  }
-  export interface Ordonnance {
-    medicamment : string,
-    appointment : AppointmentData,
-    medcine : Medicine,
-    patient : Patient
-  }
+interface Appointment {
+  dateTime: string;
+  medcine: string;
+  patient: string;
+  loginMedcine?: string;
+}
+
+export interface Ordonnance {
+  medicamment: string;
+  appointment: Appointment;
+  medcine: Medicine;
+  patient: Patient;
+}
