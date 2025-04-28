4eea29842da555d56e5f1a2812b3dedd44a94b7a
Taoufiq ELHANCHAOUI <t.elhanchaoui@gmail.com>
2025-04-28 02:14:57 +0100

    ok

--- src/pages/patient_dashboard.tsx
+++ src/pages/patient_dashboard.tsx
@@ -268,33 +268,6 @@ const PatientDashboard = () => {
                           item.status
                         )}
                       </td>
-
-                      {/* Add some style for the spinner */}
-                      <style jsx>{`
-                        .loading-spinner {
-                          display: flex;
-                          align-items: center;
-                          justify-content: center;
-                        }
-
-                        .spinner {
-                          width: 24px;
-                          height: 24px;
-                          border: 4px solid rgba(0, 0, 0, 0.1);
-                          border-left-color: #09f;
-                          border-radius: 50%;
-                          animation: spin 1s linear infinite;
-                        }
-
-                        @keyframes spin {
-                          0% {
-                            transform: rotate(0deg);
-                          }
-                          100% {
-                            transform: rotate(360deg);
-                          }
-                        }
-                      `}</style>
                     </tr>
                   ))}
                 </tbody>
