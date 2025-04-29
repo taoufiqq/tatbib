c1d413dbcd351c9769f5ecc05100fb62f73b86b2
Taoufiq ELHANCHAOUI <t.elhanchaoui@gmail.com>
2025-04-29 19:31:27 +0100

    ok

--- src/pages/list_appointments_medicine.tsx
+++ src/pages/list_appointments_medicine.tsx
@@ -248,17 +248,29 @@ const ListAppointments = () => {
       </main>
 
       <style jsx>{`
-        .spinner {
-          border: 4px solid rgba(0, 0, 0, 0.1);
-          border-left-color: orange;
-          border-radius: 50%;
-          animation: spin 1s linear infinite;
-        }
-        @keyframes spin {
-          to {
-            transform: rotate(360deg);
+         .loading-spinner {
+            display: flex;
+            align-items: center;
+            justify-content: center;
           }
-        }
+
+          .spinner {
+            width: 24px;
+            height: 24px;
+            border: 4px solid rgba(0, 0, 0, 0.1);
+            border-left-color: #ffa500;
+            border-radius: 50%;
+            animation: spin 1s linear infinite;
+          }
+
+          @keyframes spin {
+            0% {
+              transform: rotate(0deg);
+            }
+            100% {
+              transform: rotate(360deg);
+            }
+          }
         .pagination {
           margin-top: 20px;
           display: flex;
--- src/pages/secretary_dashboard.tsx
+++ src/pages/secretary_dashboard.tsx
@@ -456,23 +456,27 @@ const SecretaryDashboard: NextPage = () => {
         <style jsx>{`
           .loading-spinner {
             display: flex;
+            align-items: center;
             justify-content: center;
           }
+
           .spinner {
-            width: 20px;
-            height: 20px;
+            width: 24px;
+            height: 24px;
             border: 4px solid rgba(0, 0, 0, 0.1);
-            border-left-color: orange;
+            border-left-color: #ffa500;
             border-radius: 50%;
             animation: spin 1s linear infinite;
           }
 
           @keyframes spin {
-            to {
+            0% {
+              transform: rotate(0deg);
+            }
+            100% {
               transform: rotate(360deg);
             }
           }
-
           .pagination {
             margin-top: 20px;
             display: flex;
